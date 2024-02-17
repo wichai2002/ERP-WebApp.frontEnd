d27c3bd815439952652c4bddf3869228d3251c67
Anaphat Nuntanapiboon <88420410+anaphat64070249@users.noreply.github.com>
2024-02-17 14:23:20 +0700

    freont Frew

--- erp-client/src/views/EditPayroll.vue
+++ erp-client/src/views/EditPayroll.vue
@@ -17,7 +17,7 @@
                 <div class="row">
                     <!-- content here -->
 
-                    <div class="col-10 bg-white py-4 ms-5" style="border-radius: 10px; padding-left: 3%; padding-right: 3%;">
+                    <div class="col-10 bg-white py-4 ms-5" style="border-radius: 10px; padding-left: 3%; padding-right: 3%; margin-bottom: 5%;">
                         <div class="col-4 bg-white" style="position: absolute; top: 13%; width: 19%; left: 23%; text-align: center;">
                             <h3>EDIT PAYROLL PERSONAL</h3>
                         </div>
@@ -99,6 +99,61 @@
                             </div>
                         </div>
                     </div>
+
+
+                    <!-- edit salary base -->
+                    <div class="col-10 bg-white py-4 ms-5" style="border-radius: 10px; padding-left: 3%; padding-right: 3%; margin-bottom: 5%;">
+                        <div class="col-4 bg-white" style="position: absolute; top: 68%; width: 20%; left: 23%; text-align: center;">
+                            <h3>EDIT BASE SALARY ROLES</h3>
+                        </div>
+
+                        <div class="row g-3 align-items-center" style="border: 1px solid black; margin-top: 1%; padding-top: 3%; border-radius: 10px; padding-bottom: 3%;">
+                            <div class="col-auto">
+                                <label for="inputPassword6" class="col-form-label">Roles</label>
+                            </div>
+                            <div class="col-auto">
+                                <input type="text" id="inputPassword6" class="form-control" />
+                            </div>
+
+                            <div class="col-auto" style="margin-left: 10%;">
+                                <label for="inputPassword6" class="col-form-label">Salary Base</label>
+                            </div>
+                            <div class="col-auto">
+                                <input type="text" id="inputPassword6" class="form-control" />
+                            </div>
+
+                            <div class="row g-3 align-items-center">
+                                <div class="col-auto">
+                                    <label for="inputPassword6" class="col-form-label">Discount</label>
+                                </div>
+                                <div class="col-auto">
+                                    <input type="text" id="inputPassword6" class="form-control" />
+                                </div>
+
+                                <div class="col-auto" style="margin-left: 10%;">
+                                    <label for="inputPassword6" class="col-form-label">MOUNT DISCOUNT</label>
+                                </div>
+                                <div class="col-auto">
+                                    <input type="DATE" id="inputPassword6" class="form-control" />
+                                </div>
+                            </div>
+
+                            <div class="row g-3 align-items-center">
+                                <div class="col-auto">
+                                    <label for="inputPassword6" class="col-form-label">Bonus</label>
+                                </div>
+                                <div class="col-auto">
+                                    <input type="text" id="inputPassword6" class="form-control" />
+                                </div>
+                                <div class="col-auto" style="margin-left: 40%;">
+                                   <button class="btn btn-success">SAVE</button>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>

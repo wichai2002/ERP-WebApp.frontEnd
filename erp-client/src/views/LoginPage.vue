19bec46b0763ce95dad85589d158878c39bfc8cf
Wichai Kommongkhun <kommongkhun.wichai@gmail.com>
2024-02-18 20:19:58 +0700

    .env.local .env.*.local .env.production .env.development

--- erp-client/src/views/LoginPage.vue
+++ erp-client/src/views/LoginPage.vue
@@ -5,7 +5,10 @@
                 <div class="card py-4">
                     <!-- <img class="rounded" src="../assets/hrms_logo.png" alt="Title" width="200" /> -->
                     <div class="card-body py-4 ">
-                        <img src="../assets/hrms_logo.png" class="" alt="" width="100">
+                        <div class="d-flex justify-content-center">
+                            <img src="../assets/hrms_logo.png" alt="" width="100">
+                        </div>
+
                         <h1 style="text-align: center; font-weight: 700;" class="p-3">HRMS</h1>
                         <div class="">
                             <div class="mx-5 px-2">
@@ -13,19 +16,25 @@
                                     <span class="input-group-text" id="addon-wrapping"><i
                                             class="bi bi-person-fill"></i></span>
                                     <input type="text" class="form-control" placeholder="Username" aria-label="Username"
-                                     v-model="emp_gen_id"   aria-describedby="addon-wrapping">
+                                        v-model="emp_gen_id" aria-describedby="addon-wrapping">
                                 </div>
                                 <div class="input-group flex-nowrap mb-4">
                                     <span class="input-group-text" id="addon-wrapping"><i
                                             class="bi bi-lock-fill"></i></span>
                                     <input type="password" class="form-control" placeholder="Password" aria-label="Username"
-                                      v-model="password"  aria-describedby="addon-wrapping">
+                                        v-model="password" aria-describedby="addon-wrapping">
                                 </div>
                             </div>
-                            <div class="text-center p-2">
-                                <button class="btn btn-lg" @click="login()"
-                                    style="background-color: #2BC48A; color: aliceblue;">Login</button>
+                            <div class="row d-flex justify-content-center">
+                                <div class="text-center p-2 col-lg-6 col-mb-4 col-sm-2 ">
+                                    <div class="">
+                                        <button class="btn btn-lg" @click="login()"
+                                            style="background-color: #2BC48A; color: aliceblue;">Login</button>
+                                    </div>
+
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
@@ -38,14 +47,14 @@
 <script>
 import axios from 'axios';
 export default {
-    data(){
-        return{
-            emp_gen_id : "",
+    data() {
+        return {
+            emp_gen_id: "",
             password: ""
         }
     },
-    methods:{
-      async login(){
+    methods: {
+        async login() {
             const hrAccount = {
                 hr_id: 0,
                 emp_gen_id: Number(this.emp_gen_id),
@@ -54,9 +63,9 @@ export default {
             // console.log(`${_env.VUE_APP_PROTOCAL}://${_env.VUE_APP_HOST}:${_env.VUE_APP_PORT}/${_env.VUE_APP_API_PREFIX}/Authentication`);
             const _env = process.env;
             const authen = await axios.post(`${_env.VUE_APP_PROTOCAL}://${_env.VUE_APP_HOST}:${_env.VUE_APP_PORT}/${_env.VUE_APP_API_PREFIX}/Authentication`, hrAccount);
-            
-            if (authen.status == 200){
-                if (localStorage.getItem("emp_gen_id") || localStorage.getItem("token")){
+
+            if (authen.status == 200) {
+                if (localStorage.getItem("emp_gen_id") || localStorage.getItem("token")) {
                     localStorage.clear();
                 }
                 localStorage.setItem("emp_gen_id", authen.data.emp_gen_id);
@@ -66,10 +75,10 @@ export default {
 
                 window.location.href = window.location.hostname + "/dashboard";
                 // this.$router.push('/dashboard')
-            }else{
+            } else {
                 console.log(authen.status);
             }
-            
+
         }
     }
 }
@@ -82,4 +91,5 @@ html {
 
 .container-fluid {
     background-color: #263540;
-}</style>
+}
+</style>
